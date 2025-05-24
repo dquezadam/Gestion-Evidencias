72c97ea0d1393c0464399889dc44806c760a14dc
dquezadam <danielqm31@gmail.com>
2025-05-24 02:58:19 -0400

    actualizar paquetes de django para render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,14 +92,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestion_db',
-        'USER': 'postgres',
-        'PASSWORD': 'k1llxenn',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 CORS_ALLOWED_ORIGINS = [
