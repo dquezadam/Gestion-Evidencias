aac965f8da80d5d6509c6019952b2d995e44ad98
dquezadam <danielqm31@gmail.com>
2025-05-24 03:30:26 -0400

    Variables para render 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,14 +149,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# --- CREAR SUPERUSUARIO AUTOMÁTICAMENTE EN RENDER (solo para el primer deploy) ---
-if os.environ.get('RENDER_SUPERUSER', None) == '1':
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'k1llxenn')
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-# --- FIN BLOQUE TEMPORAL ---
